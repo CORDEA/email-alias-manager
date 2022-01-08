b1516885ce3eb1e4524f571fc06beb58ce9bcf5a
Yoshihiro Tanaka <contact@cordea.jp>
2022-01-08 22:39:05 +0900

    create http client for google apis

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	admin "google.golang.org/api/admin/directory/v1"
+	"io/ioutil"
 	"log"
+	"math/rand"
+	"net/http"
+	"os"
 )
 
 func main() {
@@ -16,5 +25,46 @@ func main() {
 		log.Fatalln("Received illegal option")
 	}
 	key := flag.Arg(0)
+
+	ctx := context.Background()
+	path := os.Getenv("GOOGLE_CLIENT_SECRET")
+	secret, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client, err := authorize(ctx, secret)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	_ = client
 	_ = key
+}
+
+func authorize(ctx context.Context, secret []byte) (*http.Client, error) {
+	config, err := google.ConfigFromJSON(secret, admin.AdminDirectoryUserAliasScope)
+	if err != nil {
+		return nil, err
+	}
+	state := generateState()
+	url := config.AuthCodeURL(state, oauth2.AccessTypeOffline)
+	fmt.Println(url)
+
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+		return nil, err
+	}
+	token, err := config.Exchange(ctx, code)
+	if err != nil {
+		return nil, err
+	}
+	return config.Client(ctx, token), nil
+}
+
+func generateState() string {
+	state := ""
+	for i := 0; i < 3; i++ {
+		state += fmt.Sprintf("%c", rand.Intn(26)+97)
+	}
+	return state
 }
