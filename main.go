b0fa9de28d582927f54cdba65b64eda6b2f8f36c
Yoshihiro Tanaka <contact@cordea.jp>
2022-01-08 21:06:51 +0900

    receive command line args

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
+)
 
+func main() {
+	list := flag.Bool("l", false, "List aliases")
+	alias := flag.String("a", "", "Add alias")
+	flag.Parse()
+	if flag.NArg() <= 0 {
+		log.Fatalln("User key is required")
+	}
+	if !*list && len(*alias) <= 0 {
+		log.Fatalln("Received illegal option")
+	}
+	key := flag.Arg(0)
+	_ = key
 }
